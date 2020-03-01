8ca3d1f1f80fd8c9eaf14b7067adb6e2169817b5
rafiudd <sirin6867@gmail.com>
2020-03-01 20:18:38 +0700

    add import point pelanggaran

--- modules/import-data/import.controller.js
+++ modules/import-data/import.controller.js
@@ -7,21 +7,34 @@ const { ERROR: httpError } = require('../../helpers/httpError');
 const response = require('../../helpers/wrapper');
 
 const Aturan = db.Aturan;
+const Point = db.Point;
 
 // routes
-router.post('/all', importAll);
+router.post('/peraturan', importPeraturan);
+router.post('/point', importPoint);
 
 module.exports = router;
 
-async function importAll(req,res) {
+async function importPeraturan(req,res) {
     try {
         let soal = require('../../data/buku-saku.json');
         let query = await Aturan.insertMany(soal);
-        console.log(JSON.stringify(query[0].pasal))
+
         return response.wrapper_success(res, 200, "Sukses Upload Peraturan", query)
     } catch (error) {
         console.log(error)
         return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')         
     }
-    
+}
+
+async function importPoint(req,res) {
+    try {
+        let point = require('../../data/daftar-point.json');
+        let query = await Point.insertMany(point);
+        
+        return response.wrapper_success(res, 200, "Sukses Upload Point", query)
+    } catch (error) {
+        console.log(error)
+        return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')         
+    }
 }

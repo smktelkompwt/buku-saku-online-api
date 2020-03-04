fb82c5125d50b0e98befc6fade7545e8a4fc467d
rafiudd <sirin6867@gmail.com>
2020-03-04 16:22:40 +0700

    fix upload with minio

--- modules/lapor/lapor.controller.js
+++ modules/lapor/lapor.controller.js
@@ -5,9 +5,12 @@ const router = express.Router();
 // import helper
 const config = require('../../config.json');
 const db = require('../../helpers/db');
+const wrapper = require('../../helpers/wrapper');
 const { ERROR: httpError } = require('../../helpers/httpError');
 const response = require('../../helpers/wrapper');
 const uploadBase64 = require('../../helpers/uploadBase64');
+const uploads = require('../../helpers/uploadBase64');
+const minio = require('../../helpers/minioSdk');
 
 const User = db.User;
 const Point = db.Point;
@@ -16,6 +19,9 @@ const Aktivitas = db.Aktivitas;
 
 // routes
 router.post('/upload', uploadPelanggaran);
+router.get('/all', getAllPelanggaran);
+router.get('/', getPelanggaranByid);
+router.delete('/delete', deleteAllLaporan);
 module.exports = router;
 
 async function uploadPelanggaran(req,res) {
@@ -40,10 +46,31 @@ async function uploadPelanggaran(req,res) {
         if(getPelanggaran.length < 1) {
             return response.wrapper_error(res, httpError.NOT_FOUND, 'Pelanggaran tidak ditemukan')         
         }
+        
+        const bucket = "pelanggaran"
+        let checkBucket = await minio.isBucketExist(bucket);
 
-        let image = await uploadBase64(req.body.image);
-        image.replace(/\./g,' ')
-        let path = "localhost:3000/buku-saku-online-api"
+        if(!checkBucket) {
+          await minio.bucketCreate(bucket);
+        }
+        
+        const objectName = {
+            folder: `${getUser[0].nis}`,
+            filename: Date.now()
+        };
+      
+        const uploadPhoto = await uploads.uploadImages(bucket, req.body.image, objectName);
+      
+        if (uploadPhoto.err) {
+          return wrapper.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong');
+        }
+        const minioEndpoint = "localhost"
+        const port = 9000;
+        let resultImage = `${minioEndpoint}:${port}/${uploadPhoto}`
+      
+        // let image = await uploadBase64(req.body.image);
+        // image.replace(/\./g,' ')
+        // let path = "localhost:3000/buku-saku-online-api"
 
         let model = {
             user: {
@@ -60,7 +87,7 @@ async function uploadPelanggaran(req,res) {
                 id: getPelapor[0]._id,
                 nama: getPelapor[0].name
             },
-            image: path + image,
+            image: resultImage,
        }    
        let lapor = new Lapor(model)
        let query = await lapor.save();
@@ -70,5 +97,36 @@ async function uploadPelanggaran(req,res) {
         console.log(error)
         return response.wrapper_error(res, httpError.INTERNAL_ERROR, error)         
         
+    }
+}
+
+async function getAllPelanggaran(req,res) {
+    try {
+        let query = await Lapor.find();
+        return response.wrapper_success(res, 200, "Sukses Get Pelanggaran", query)
+    } catch (error) {
+        return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')         
+    }   
+}
+
+async function getPelanggaranByid(req, res) {
+    try {
+        let model = {
+            _id : req.query.id
+        }
+    
+        let query = await Lapor.findById(model._id);
+        return response.wrapper_success(res, 200, "Sukses Get Peraturan Peraturan by id", query)
+    } catch (error) {
+        return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')                 
+    }
+}
+
+async function deleteAllLaporan(req,res) {
+    try {
+        let query = await Lapor.remove();
+        return response.wrapper_success(res, 200, "Sukses Hapus All Peraturan", query)
+    } catch (error) {
+        return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')                         
     }
 }

05987041db117c5a87e7b3aeca48790fae8d665c
rafiudd <sirin6867@gmail.com>
2020-03-09 19:35:52 +0700

    add jurusan and add deleteAll kelas

--- modules/kelas/kelas.controller.js
+++ modules/kelas/kelas.controller.js
@@ -13,12 +13,15 @@ const User = db.User;
 router.get('/all', getAll);
 router.post('/create', create);
 router.get('/', getUserInClass);
+router.delete('/delete', _delete);
 
 module.exports = router;
 
 async function getAll(req,res) {
     try {
-        let query = await Kelas.find();
+        let jurusan = req.query.jurusan;
+
+        let query = await Kelas.find({ 'jurusan': jurusan });
         return response.wrapper_success(res, 200, "Sukses Get All Kelas", query)
     } catch (error) {
         return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')         
@@ -29,7 +32,8 @@ async function create(req,res) {
     try {
         let model = {
             kelas: req.body.kelas,
-            wali_kelas: req.body.wali_kelas
+            wali_kelas: req.body.wali_kelas,
+            jurusan: req.body.jurusan
         }
 
         let kelas = new Kelas(model);
@@ -56,4 +60,14 @@ async function getUserInClass(req,res) {
     } catch (error) {
         return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')                         
     }
+}
+
+async function _delete(req, res) {
+    try {
+        let query = await Kelas.remove();
+        return response.wrapper_success(res, 200, "Sukses Hapus Kelas", query)
+    } catch (error) {
+        return response.wrapper_error(res, httpError.INTERNAL_ERROR, 'Something is wrong')                         
+    }
+   
 }
--- modules/kelas/kelas.model.js
+++ modules/kelas/kelas.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     kelas: { type: String },
     wali_kelas: { type: String },
+    jurusan: { type: String },
     createdDate: { type: Date, default: Date.now }
 });
 
